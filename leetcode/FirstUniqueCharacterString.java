7dff9212009fc8112232e02bdb9480ba05c09f36
Yash Rakesh <82806001+dev-yashrakesh@users.noreply.github.com>
2023-11-20 23:00:02 +0530

    Update FirstUniqueCharacterString.java

--- leetcode/FirstUniqueCharacterString.java
+++ leetcode/FirstUniqueCharacterString.java
@@ -2,22 +2,17 @@ package leetcode;
 
 public class FirstUniqueCharacterString {
 
-public int firstUniqChar(String s) {
+class Solution {
+    public int firstUniqChar(String s) {
         for(int i=0;i<s.length();i++) {
-        	int count=0;
-        	for(int j=0;j<s.length();j++) {
-        		if(s.charAt(i)==s.charAt(j)) {
-        			count++; 
-        		}
-        		
-        	}
-        	if(count==1) {
+        	int temp=s.charAt(i);
+        	if(s.lastIndexOf(temp)==s.indexOf(temp)) {
         		return i;
         	}
-        	
         }
         return -1;
     }
+}
 	
 	public static void main(String[] args) {
 		String s="aba";
