c38e620a21965b89d3d34d6fa75e22787c54f858
Yash Rakesh <82806001+dev-yashrakesh@users.noreply.github.com>
2024-01-27 21:29:27 +0530

    Add files via upload

---
+++ leetcode/CountCompleteTreeNode.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class CountCompleteTreeNode {
+
+public int countNodes(TreeNode root) {
+        if(root==null) {
+        	return 0;
+        }
+        
+        int left=countNodes(root.left);
+        int right=countNodes(root.right);
+        
+        return 1+left+right;
+    }
+	
+}
